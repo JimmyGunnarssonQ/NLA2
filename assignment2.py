2bface53d0f7a10d05f52d6c2bf72de573036f6f
Jimmy Kornelije Gunnarsson <79020745+JimmyGunnarssonQ@users.noreply.github.com>
2022-09-09 16:02:17 +0200

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -8,19 +8,21 @@ class ortho:
         self.mval, self.nval = self.matrix.shape
 
     def gramschmidt(self):
+        '''Gram Schmidt method, generates an orthogonal matrix Q '''
         a0 = self.matrix[:,0].tolist()
         a0/=nl.norm(a0)
-        Q = [a0]
+        Q = [a0] #initial vector 
         for i in range(1,self.nval):
-            a = self.matrix[:,i].tolist()
-            newvec = (a - sum(np.multiply(u, np.dot(u,a)) for u in Q)).tolist()
-            newvec/=nl.norm(newvec)
-            Q.append(newvec)
-        Q = np.array(Q).transpose()
+            a = self.matrix[:,i].tolist() #pegging value 
+            newvec = (a - sum(np.multiply(u, np.dot(u,a)) for u in Q)).tolist() #gram Schmidt method 
+            newvec/=nl.norm(newvec) #normalisation 
+            Q.append(newvec) #appending the new column vector 
+        Q = np.array(Q).transpose() #transpose is needed because we work with row vector internally 
 
         return Q
     
     def devi(self):
+        ''' test run for deviations'''
         GSmat = self.gramschmidt()
         numident = np.matmul(GSmat.transpose(), GSmat) 
 
@@ -32,26 +34,24 @@ class ortho:
         return f"2-nl.norm deviation: {twonl.norm} \n2-nl.norm deviation from identity: {ident} \nrange of eigenvalues: ({min(eigenvalues)}, {max(eigenvalues)}) \ndeterminant: {determinant}"
     
     def householder(self):
-        
+        '''Householder method, used to generate an upper triangular matrix R '''
+        dummymatrix = self.matrix #dummy matrix to be used for Householder reflections
+        R = self.matrix #we fill it up with Householder reflected submatricies 
         for i in range(self.nval):
-            x = self.matrix[:,i]
-            print(x)
-            newvec = [sign(x[0])*nl.norm(x)]
-            for j in range(x.shape[0]-1):
-                newvec.append(0)
-            v = newvec + x 
+            a = dummymatrix[:,0] #vector to be modified for reflection
+            mpart, npart = dummymatrix.shape
+            v = a- np.array([nl.norm(a)] + (mpart-1)*[0]) #vector used for reflection axis
+            v/=nl.norm(v) #normalisation 
+            Q = np.eye(mpart) - np.multiply(np.outer(v,v),2) #reflection matrix 
+            dummymatrix = np.matmul(Q, dummymatrix) #performing the Household reflection
+            R[i:,i:] = dummymatrix #updating values
+            dummymatrix = dummymatrix[1:, 1:] #new itteration matrix submatrix with dim m-1 x n-1 
+        return R
 
 
-def sign(x):
-    if x<0:
-        s=-1 
-    else:
-        s=1
-    return s 
-val = 3
+
+'''Some testing values for verifications '''
+val = 5
 matin = nr.rand(val+2,val)
 mat = ortho(matin)
-
-#print(matin)
-#q,_ = sl.qr(matin, mode = "economic")
-#print(q)
+print(mat.householder())
